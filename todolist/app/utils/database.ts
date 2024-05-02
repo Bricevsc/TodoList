17261c6f54a483f988c2fce54d792c3572b48ff2
brice <prodakil@gmail.com>
2024-05-02 15:41:46 +0200

    ✨ add connection to db

---
+++ todolist/app/utils/database.ts
@@ -0,0 +1,23 @@
+import mongoose, { ConnectOptions } from "mongoose";
+
+let isConnected: boolean = false
+
+export const connectToDB = async()=>{
+    mongoose.set('strictQuery', true)
+
+    if (isConnected){
+        console.log('MongoDb is connected')
+        return;
+    }
+    try{
+        await mongoose.connect(
+            process.env.MONGODB_URI!,{
+            dbName:'tasks',
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            } as ConnectOptions
+        )
+    }catch(error){
+        console.log(error)
+    }
+}
