2929932e31b0d6d8c8a7bcab8fbd2760c3a4a40a
brice <prodakil@gmail.com>
2024-05-02 17:19:19 +0200

    🗃️ query all tasks

--- todolist/app/page.tsx
+++ todolist/app/page.tsx
@@ -1,13 +1,29 @@
 "use client";
+import { Flex, Spinner } from "@chakra-ui/react";
 
 import Header from "./components/Header";
+import Task from "./components/Task";
 import AddTask from "./components/AddTask";
+import NoTasks from "./components/NoTasks";
 
 import { useState, useEffect } from "react";
+import { ITask } from "./types";
 
 export default function Home() {
   const [task, setTask] = useState("");
   const [isLoadind, setIsLoading] = useState(true);
+  const [allTasks, setAllTasks] = useState([]);
+
+  const fetchTasks = async () => {
+    try {
+      const response = await fetch("/api/task/all");
+      const data = await response.json();
+      setAllTasks(data);
+      setIsLoading(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   const handleCreateTask = async () => {
     setIsLoading(true);
@@ -20,6 +36,7 @@ export default function Home() {
       });
       if (response.ok) {
         setTask("");
+        fetchTasks();
       } else {
         console.log("error");
       }
@@ -29,6 +46,14 @@ export default function Home() {
     setIsLoading(false);
   };
 
+  const handleCompleteTask = async () => {};
+
+  const handleDeleteTask = async () => {};
+
+  useEffect(() => {
+    fetchTasks();
+  }, []);
+
   return (
     <>
       <Header />
@@ -37,6 +62,26 @@ export default function Home() {
         setTask={setTask}
         handleCreateTask={handleCreateTask}
       />
+      {isLoadind ? (
+        <Spinner />
+      ) : (
+        <>
+          <Flex direction="column" p="2rem">
+            {allTasks.length > 0 ? (
+              allTasks.map((individualTask: ITask) => (
+                <Task
+                  key={individualTask._id}
+                  individualTask={individualTask}
+                  handleCompleteTask={handleCompleteTask}
+                  handleDeleteTask={handleDeleteTask}
+                />
+              ))
+            ) : (
+              <NoTasks />
+            )}
+          </Flex>
+        </>
+      )}
     </>
   );
 }
